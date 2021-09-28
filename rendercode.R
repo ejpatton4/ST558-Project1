aa77694ffcee42aa695fe80a152426af6ab30e3b
Evan Patton <ejpatton4@gmail.com>
2021-09-28 17:25:31 -0400

    first serious commit

--- rendercode.R
+++ rendercode.R
@@ -1,3 +1,3 @@
 library(rmarkdown)
 
-render("tesfile.rmd",output_file= "README.md", output_format = "github_document")
+render("poke_api_vignette.rmd",output_file= "README.md", output_format = "github_document")
