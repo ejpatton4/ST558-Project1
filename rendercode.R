be39fb3d5ab5e94d25c4f3aa9c279717e7cf235e
Evan Patton <ejpatton4@gmail.com>
2021-09-28 23:33:10 -0400

    stats function being tested, plan created for rest of time.

--- rendercode.R
+++ rendercode.R
@@ -1,3 +1,5 @@
 library(rmarkdown)
 
 render("poke_api_vignette.rmd",output_file= "README.md", output_format = "github_document")
+
+render("plan.rmd", output_file = "plan.md", output_format = "github_document")
--- testing_formulas.R
+++ testing_formulas.R
@@ -3,105 +3,37 @@ library(jsonlite)
 library(tidyverse)
 
 
-encounter <- function(poke, game = "all") {
+90.5/(1.7^2)
 
-poke <- tolower(poke)
-game <- tolower(game)
+raw_data <- fromJSON("https://pokeapi.co/api/v2/pokemon/charizard", flatten = TRUE)
+
+
+df <- raw_data[["stats"]]
+
+df$name <- str_to_sentence("charizard")
+
+df <- df[,c(5,1,3)]
+
+df <- df %>%
+  pivot_wider(names_from = stat.name, values_from = base_stat)
+
+
+# This function retrieves the base stats for a given pokemon, or hopefully vector of pokemon.
+
+stats <- function(poke) {
   
-raw_data <- fromJSON(paste0("https://pokeapi.co/api/v2/pokemon/",poke,"/encounters"), flatten = TRUE)
-
-
-# this function is designed for getting where to find specific Pokemon
-for (i in 1:dim(raw_data)[1]) {
+  raw_data <- fromJSON(paste0("https://pokeapi.co/api/v2/pokemon/",poke), flatten = TRUE)
   
-  loc <- raw_data[i,2]
+  df <- raw_data[["stats"]]
   
-
-
-  for (b in 1:dim(raw_data[[1]][[i]])[1]){
-    
-    gm <- raw_data[[1]][[i]][b,3]
-    
-    
-    for (a in 1:dim(raw_data[[1]][[i]][[1]][[b]])[1]){
-      
-      chance <- raw_data[[1]][[i]][[1]][[b]][a,1]
-      max    <- raw_data[[1]][[i]][[1]][[b]][a,3]
-      min    <- raw_data[[1]][[i]][[1]][[b]][a,4]
-      meth   <- raw_data[[1]][[i]][[1]][[b]][a,5]
-      line   <- c(loc,gm,chance,max,min,meth)
-
-      if(a == 1){
-        dfa <- tibble(loc,gm,chance,max,min,meth)
-      } else{
-        #if(a<=(dim(raw_data[[1]][[i]][[1]][[b]])[1])){
-          dfa <- rbind(dfa,line)
-        #}else{
-         # dfa <- tibble(dfa,line)
-          #dfa <- distinct(dfa)
-        #}
-        }
-      }
+  df$name <- str_to_sentence(poke)
   
-    if(b == 1)
-    {dfg <- dfa
-    }else{
-      dfg <- rbind(dfg,dfa)
-    }
-    
-    
-    }
-    
-
+  df <- df[,c(5,1,3)]
   
-
-  if(i==1){
-    df <- dfg
-  }else{
-    df <- rbind(df,dfg)
-  }
-  }
-
-names(df) <- c("Location","Game","Chance","Max_Level","Min_Level","Method")
-
-df <- distinct(df)
-
-if(game == "all"){
-   return(df)
-}else{
-    return(df %>%
-             filter(Game == game))
-  } #else close
-
-} #func close
-
-
-
-
-
-ditto <- encounter("ditto")
-ditto2 <- encounter("ditto","RED")
-
-char <- encounter("charmander")
-
-
-mons <- list("charmander","squirtle","bulbasaur")
-
-hope <- lapply(mons, FUN = encounter, game ="all")
-
-hope2 <- tibble(unlist(hope[1]))
-
-hope2 <- unlist(hope)
-
-rattata <- GET("https://pokeapi.co/api/v2/pokemon/ditto/encounters")
-
-rattata_data <- rattata$content
-
-rattata_data <- rawToChar(rattata_data)
-
-rattata_data <- fromJSON(rattata_data,  flatten = TRUE)
-
-rattata_data[[1]][[1]][[1]][[1]][[1]]
-
-
-    
+  df <- df %>%
+    pivot_wider(names_from = stat.name, values_from = base_stat)
+  
+  return(df)
+  
+  
+}
